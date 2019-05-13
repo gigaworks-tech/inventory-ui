d6bf4d774d1de744845186a575208fb9379ccb6d
Anurag Gupta <agupta6@mac18j-c3.chec.local>
2019-05-13 18:15:34 +0530

    Anurag::Added the basic routing

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,22 @@
-console.log("It Works");
+import React from 'react';
+import { render } from 'react-dom';
+import { Footer } from '../component/Footer';
+import { Header } from '../component/Header';
+import { Home } from '../component/Home';
+import {Route, BrowserRouter, Switch} from "react-router-dom";
+class App extends React.Component {
+    render() {
+        return (
+                <BrowserRouter >
+                <Switch>
+                    <Route exact path={"/"} component={Header}/>
+                    <Route path={"/"} component={Header}/>
+                </Switch>    
+                    <Route path = {"/header"} component={Header} />
+                    <Route path = {"/footer"} component={Footer} />
+                </BrowserRouter>
+        );
+    }
+}
+
+render(<App/>,window.document.getElementById("app"))
---
+++ src/component/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+export class Footer extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1> Footer </h1>
+                </div>
+        )
+    }
+}
