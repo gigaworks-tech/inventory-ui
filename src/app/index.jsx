989472ddd7655f84df0304343e751ad9592f9ee8
Anurag Gupta <agupta6@Macbook-pro-2.local>
2019-05-15 00:55:09 +0530

    Anurag::Fix the import issue

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import Footer from '../component/Footer';
-import Header from '../component/Header';
 import Home from '../component/Home';
 import MainPage from '../container/MainPage';
 import store from '../store';
